98245e51d844d716233b3993f65985e1b82e8602
Tomoaki Hirata <t.hirata.0463@gmail.com>
2021-02-16 21:42:06 +0900

    test case section finish

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,5 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] # headlessでブラウザを非表示に設定
+  include SignInHelper
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+require_relative 'sign_in_helper'
 require 'rails/test_help'
+require 'minitest/mock'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
