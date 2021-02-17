b1a7d5423ae650cc47eae61cd6dc23aca8fc8815
Tomoaki Hirata <t.hirata.0463@gmail.com>
2021-02-18 00:54:57 +0900

    500 check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
   helper_method :logged_in?, :current_user
 
-  rescue_from Exception, with: :error500
-  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+  # rescue_from Exception, with: :error500
+  # rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
 
   private
 
